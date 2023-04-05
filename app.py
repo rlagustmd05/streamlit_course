5bbda6b93df0aaeb0aec43698f39d2c229949281
KHS <kjhee58888@naver.com>
2023-04-05 20:42:40 +0900

    2nd

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title('Streamlit Course')
+st.header('Hello :blue[Streamlit]!!! :sunglasses:')
+
+code = '''def Hello():
+    st.header('Hello :blue[Streamlit]!!! :sunglasses:')
+    '''
+st.code(code, language='python')
+
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
